691cdd34063596d6e3da3f8a2566e622f9e449de
久久 <2579709381@qq.com>
2019-08-16 13:34:39 +0800

    commit

--- itoken-common-service/src/main/java/tk/mybatis/mapper/MyMapper.java
+++ itoken-common-service/src/main/java/tk/mybatis/mapper/MyMapper.java
@@ -1,7 +1,7 @@
 package tk.mybatis.mapper;
 
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
+import tk.mybatis.mapper.Mapper;
+import tk.mybatis.mapper.MySqlMapper;
 
 /**
  * 自己的 Mapper
